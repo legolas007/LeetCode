d558d11db8375fa6390ec564d78d3dbd966aee43
legolas007 <oukuanou@163.com>
2018-03-10 10:42:08 +0800

    update

---
+++ ArrangingCoins.java
@@ -0,0 +1,12 @@
+package main.java.com.usher.algorithms.array;
+
+public class ArrangingCoins {
+    public int arrangeCoins(int n) {
+        int lev = 1;
+        while (n > 0){
+            n -= lev;
+            lev++;
+        }
+        return n == 0? lev-1:lev-2;
+    }
+}
