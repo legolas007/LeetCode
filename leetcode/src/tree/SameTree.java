d18349652a9e587191ae676e827b832aeded802c
legolas007 <oukuanou@163.com>
2018-05-10 17:46:08 +0800

    tree

---
+++ leetcode/src/tree/SameTree.java
@@ -0,0 +1,20 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: Usher
+ * @Description:
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null && q != null || (p != null && q == null))
+            return false;
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right ,q.right);
+    }
+}
