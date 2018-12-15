2b767749cdec56bdef3f880e8d1c71dff20a9df5
Usher <oukuanou@163.com>
2018-12-15 16:24:11 +0800

    fix

--- leetcode/src/array/CompareStrings.java
+++ leetcode/src/array/CompareStrings.java
@@ -14,11 +14,14 @@ public class CompareStrings {
         char[] B1 = B.toCharArray();
         HashMap<Character,Integer> hm = new HashMap<>();
         for (char c : A1){
+            /**
             if(!hm.containsKey(c)){
                 hm.put(c,1);
             }else {
                 hm.put(c,hm.get(c)+1);
             }
+            */
+            hm.merge(c,1,Integer::sum);
         }
 
         for (char c : B1){
